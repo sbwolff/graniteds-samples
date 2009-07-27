1d4c1f73586d39262b1b924870fb6c06cdb9ba52
anthibug <anthibug@6f0b6db0-715c-11de-a9df-91707ec8e704>
2009-07-27 11:59:26 +0000

    Replace secure spring method with standards-compliant annotation @RolesAllowed from JSR-250

--- tide-test/tide-test-server/src/main/java/org/granite/tide/test/services/impl/LoginManagerImpl.java
+++ tide-test/tide-test-server/src/main/java/org/granite/tide/test/services/impl/LoginManagerImpl.java
@@ -2,7 +2,7 @@ package org.granite.tide.test.services.impl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.security.annotation.Secured;
+import javax.annotation.security.RolesAllowed;
 import org.springframework.stereotype.Service;
 
 import org.granite.tide.test.domain.model.User;
@@ -17,11 +17,11 @@ public class LoginManagerImpl implements LoginManager {
 	/* (non-Javadoc)
 	 * @see com.atosorigin.services.impl.UserManager#login(java.lang.String, java.lang.String)
 	 */
-	@Secured("ROLE_ADMIN")
+	@RolesAllowed("ROLE_ADMIN")
 	public User login(String login, String pass) throws Exception {
 		
 		if (logger.isDebugEnabled()) logger.debug("IN LOGIN");
-				
+		
 		User user = null;
 		
 		if (login.equals("admin")){
@@ -41,7 +41,7 @@ public class LoginManagerImpl implements LoginManager {
 		return user;
 	}
 
-	@Secured("ROLE_ADMIN")
+	@RolesAllowed("ROLE_ADMIN")
 	public String sayHello() {
 		if (logger.isDebugEnabled()) logger.debug("IN TEST");
 		return "hello";
